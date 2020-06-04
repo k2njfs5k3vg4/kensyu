bff9205a906b01add4882944ef31991bdc6c9508
Makoto Takei <makoto.takei.tm@nttdocomo.com>
2020-06-04 14:12:44 +0900

    Sample2 Commit

---
+++ src/kensyumaster/Simple2.java
@@ -0,0 +1,29 @@
+package kensyumaster;
+
+class A2{
+	int x;
+	A2 (int x){
+		this.x = x;
+		System.out.println("クラスAのコンストラクタを実行しました。");
+	}
+}
+
+class B2 extends A2 {
+	int y;
+
+	B2(){
+		super(10);
+		y = 20;
+		System.out.println("クラスBのコンストラクタを実行しました。");
+	}
+}
+
+public class Simple2 {
+
+	public static void main(String[] args) {
+		B2 b = new B2();
+		System.out.println("bのxの値は"+b.x+"、yの値は"+b.y+"です。");
+
+	}
+
+}
