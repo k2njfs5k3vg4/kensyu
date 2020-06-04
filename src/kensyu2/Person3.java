3854dbe9bfa215561281572d5046b5b148d96cb4
Mayu Hosodo <dcmsd08095540442@gmail.com>
2020-06-04 16:56:57 +0900

    create Person3.java

---
+++ src/kensyu2/Person3.java
@@ -0,0 +1,32 @@
+package kensyu2;
+
+public class Person3 {
+
+	private String name;
+
+	public Person3(String string) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.name = string;
+	}
+
+	public Person3() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public String getName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return name;
+	}
+
+	public void setName(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		this.name = string;
+
+	}
+
+	public String introduce() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "私は" + name + "と申します。";
+	}
+
+}
