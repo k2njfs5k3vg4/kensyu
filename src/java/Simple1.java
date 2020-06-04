9c365d6335babd0a8059cd3c35968fba4e019f56
Hiroki Kameda <hiroki.kameda.nc@nttdocomo.com>
2020-06-04 14:25:19 +0900

    add Simple1.java

---
+++ src/java/Simple1.java
@@ -0,0 +1,29 @@
+package java;
+
+class A {
+	int x;
+
+	A() {
+		x = 10;
+		System.out.println("クラスAのコンストラクタを実行しました。");
+	}
+}
+
+class B extends A {
+	int y;
+
+	B() {
+		y = 10;
+		System.out.println("クラスBのコンストラクタを実行しました。");
+	}
+}
+
+public class Simple1 {
+
+	public static void main(String[] args) {
+		B b = new B();
+		System.out.println("x:" + b.x + "y:" + b.y);
+
+	}
+
+}
