e786a5aad55267c4d54b1d84f06f4304c27d2477
Aoi Uchino <aoi.uchino.ct@nttdocomo.com>
2020-06-04 14:07:31 +0900

    "create Simple1.java"

---
+++ src/kensyu2/Simple1.java
@@ -0,0 +1,25 @@
+package kensyu2;
+
+class A {
+	int x;
+	A(){
+		x = 10;
+		System.out.println("クラスAのコンストラクタを実行しました。");
+	}
+}
+
+class B extends A {
+	int y;
+	B(){
+		y = 20;
+		System.out.println("クラスBのコンストラクタを実行しました。");
+	}
+}
+
+ class Simple1 {
+	public static void main(String[] args) {
+		B b = new B();
+		System.out.println("bのxの値は"+ b.x + "、yの値は" + b.y + "です");
+	}
+
+}
