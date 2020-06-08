8dd0d3cc4cd1ed9a464d8670facc0d37c92ae2de
Takaki Kawamura <docosys.r2kawamura@gmail.com>
2020-06-08 10:47:29 +0900

    ExceptionTest4 commit

---
+++ src/chapter16/ExceptionTest4.java
@@ -0,0 +1,29 @@
+package chapter16;
+
+public class ExceptionTest4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			System.out.println("try文中");
+			int[] a = new int[2];
+			a[0] = 5;
+			a[2] = 4;
+			int i = 10, j = 0;
+			int k = i / j;
+			System.out.println("K = " + k);
+		} catch (ArithmeticException e) {
+			System.out.println("catch文中");
+			System.out.println("エラー：0で除算しました");
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("catch文中");
+			System.out.println("配列のインデックスをオーバーしました");
+			e.printStackTrace();
+		} finally {
+			System.out.println("finally文中");
+		}
+		System.out.println("プログラム終了");
+	}
+
+}
