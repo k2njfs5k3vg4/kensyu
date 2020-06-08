73eeb55fc98aa3e16068b41c1e04874c33fdaf8c
Mayu Hosodo <dcmsd08095540442@gmail.com>
2020-06-08 10:47:25 +0900

    add Exception4

---
+++ src/kensyu/ExceptionTest4.java
@@ -0,0 +1,30 @@
+package kensyu;
+
+public class ExceptionTest4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			System.out.println("try文中");
+			int[] a = new int[2];
+			a[0] = 5;
+			a[2] = 4;
+			int i = 10, j = 0;
+			int k = i / j;
+			System.out.println("K = " + k);
+		} catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+			System.out.println("catch文中");
+			//System.out.println("エラー：0で除算されました");
+			e.printStackTrace();
+		} /* catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("catch文中");
+			System.out.println("配列のインデックスをオーバーしました");
+			e.printStackTrace();
+			}*/ finally {
+			System.out.println("finally文中");
+		}
+		System.out.println("プログラム終了");
+
+	}
+
+}
