f1a64e4a207f59241709da62eb396fd65076c342
Hayato Kima <hayatokma@gmail.com>
2020-06-08 12:05:28 +0900

    Day10とInvalidDayExceptionの作成

---
+++ src/kensyu/InvalidDayException.java
@@ -0,0 +1,7 @@
+package kensyu;
+
+public class InvalidDayException extends Exception {
+	public InvalidDayException(String message) {
+		super(message);
+	}
+}
---
+++ src/kensyu/UseDay10.java
@@ -0,0 +1,50 @@
+package kensyu;
+
+class Day10 {
+	private int year;
+	private int month;
+	private int date;
+
+	@Override
+	public String toString() {
+		return year + "年" + month + "月" + date + "日";
+	}
+
+	public Day10(int year, int month, int date) throws InvalidDayException {
+		if (year < 1 || year > 3000) {
+			throw new InvalidDayException("年の範囲を超えました");
+		}
+		if (month < 1 || month > 12) {
+			throw new InvalidDayException("月の範囲を超えました");
+		}
+		if (date < 1 || date > 31) {
+			throw new InvalidDayException("日の範囲を超えました");
+		}
+		this.year = year;
+		this.month = month;
+		this.date = date;
+	}
+
+	public Day10(int year, int month) throws InvalidDayException {
+		this(year, month, 1);
+	}
+}
+
+public class UseDay10 {
+
+	public static void main(String[] args) {
+		try {
+			Day10 d1 = new Day10(2007, 12, 30);
+			Day10 d2 = new Day10(-1995, 3, 5);
+			Day10 d3 = new Day10(2000, 1000);
+
+			System.out.println(d1);
+			System.out.println(d2);
+			System.out.println(d3);
+
+		} catch (InvalidDayException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
