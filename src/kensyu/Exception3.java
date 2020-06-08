30a6ca03439284dc21cbc20599e4eb104c40acb7
Hayato Kima <hayatokma@gmail.com>
2020-06-08 10:11:57 +0900

    例外処理を作成

---
+++ src/kensyu/Exception3.java
@@ -0,0 +1,18 @@
+package kensyu;
+
+public class Exception3 {
+
+	public static void main(String[] args) {
+		try {
+			int i = 10, j = 0;
+			int k = i / j;
+			System.out.println("K = " + k);
+
+		} catch (ArithmeticException e) {
+			System.out.println("エラー:0で割りました");
+			e.printStackTrace();
+		}
+		System.out.println("main終了");
+	}
+
+}
