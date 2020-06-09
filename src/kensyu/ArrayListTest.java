df7de99436b7bb25665f76912683d5c5500396a7
Hayato Kima <hayatokma@gmail.com>
2020-06-09 13:02:25 +0900

    イテレータを試した

--- src/kensyu/ArrayListTest.java
+++ src/kensyu/ArrayListTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -49,6 +50,21 @@ class ArrayListTest {
 		Assertions.assertEquals("木村", map.get("A13").getName());
 		Assertions.assertEquals("山田", map.get("A12").getName());
 		Assertions.assertEquals("田中", map.get("A14").getName());
+	}
+
+	@Test
+	void test4() {
+		Map<String, Person3> map = new HashMap<String, Person3>();
+
+		map.put("A13", new Person3("木村"));
+		map.put("A12", new Person3("山田"));
+		map.put("A14", new Person3("田中"));
+
+		Set<String> keys = map.keySet();
+		for (String key : keys) {
+			map.get(key).introduce();
+		}
+
 	}
 
 }
