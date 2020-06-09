dcdf12919a4c60a8bc5ddaddc3079ae85b3615d3
Liang <08022789396@docomo.ne.jp>
2020-06-09 11:24:34 +0900

    add Person3

---
+++ src/kensyu/Person3.java
@@ -0,0 +1,26 @@
+package kensyu;
+
+public class Person3 {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Person3(String name) {
+		this.name = name;
+	}
+
+	public Person3() {
+
+	}
+
+	public void introduce() {
+		System.out.println("私は" + name + "と申します。");
+	}
+
+}
---
+++ src/kensyu/Person3Test.java
@@ -0,0 +1,26 @@
+package kensyu;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Person3Test extends Person3 {
+
+	@Test
+	void test() {
+		//		fail("まだ実装されていません");
+		Map<String, Person3> p = new HashMap<String, Person3>();
+
+		p.put("A13", new Person3("木村"));
+		p.put("A12", new Person3("山田"));
+		p.put("A14", new Person3("田中"));
+
+		Set<String> keys = p.keySet();
+
+		Assertions.assertEquals("木村", p.get("A13"));
+	}
+
+}
