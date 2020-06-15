470d5b0172351d619b18b7362d67b4ef8b5384ac
Makoto Takei <makoto.takei.tm@nttdocomo.com>
2020-06-15 10:58:40 +0900

    add SelectTest

---
+++ src/kensyu/SelectTest.java
@@ -0,0 +1,31 @@
+package kensyu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		String url = "jdbc:postgresql:sample";
+		String user = "student";
+		String pass = "himitu";
+		String sql = "SELECT * FROM emp";
+
+		try(Connection con = DriverManager.getConnection(url,user,pass);
+				PreparedStatement st = con.prepareStatement(sql);
+				ResultSet rs = st.executeQuery();){
+
+			while(rs.next()) {
+				System.out.print(rs.getInt("code")+"：");
+				System.out.print(rs.getString("name")+"：");
+				System.out.print(rs.getInt("age")+"：");
+				System.out.println(rs.getString("tel"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+}
