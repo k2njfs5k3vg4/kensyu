b98e636bd991c5e47228c66fa85e680fc0a07dee
Takaki Kawamura <docosys.r2kawamura@gmail.com>
2020-06-15 11:58:21 +0900

    10_2 commit

---
+++ src/kensyu/Lesson10_2.java
@@ -0,0 +1,76 @@
+package kensyu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Lesson10_2 {
+
+	private final String url = "jdbc:postgresql:sample";
+	private String user;
+	private String pass;
+
+	private int code;
+	private String name;
+	private int age;
+	private String tel;
+
+	public Lesson10_2(String user, String pass) {
+		this.user = user;
+		this.pass = pass;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getInsertValues() {
+		return code + "：" + name + "：" + age + "：" + tel;
+	}
+
+	public String insert() {
+		String sql = "INSERT INTO emp VALUES(?,?,?,?)";
+		int rows = 0;
+		try (Connection con = DriverManager.getConnection(url, user, pass);
+				PreparedStatement st = con.prepareStatement(sql);) {
+			st.setInt(1, code);
+			st.setString(2, name);
+			st.setInt(3, age);
+			st.setString(4, tel);
+			rows = st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rows + "件挿入しました。";
+	}
+
+}
---
+++ src/kensyu/Lesson10_2Test.java
@@ -0,0 +1,30 @@
+package kensyu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Lesson10_2Test {
+
+	@Test
+	@DisplayName("セッター、ゲッターの実装")
+	void test1() {
+		Lesson10_2 db = new Lesson10_2("student", "himitu");
+		db.setCode(10);
+		db.setName("Eric");
+		db.setAge(49);
+		db.setTel("0975-22-7777");
+		Assertions.assertEquals("10：Eric：49：0975-22-7777", db.getInsertValues());
+	}
+
+	@Test
+	@DisplayName("インサートメソッドの実装")
+	void test2() {
+		Lesson10_2 db = new Lesson10_2("student", "himitu");
+		db.setCode(10);
+		db.setName("Eric");
+		db.setAge(49);
+		db.setTel("0975-22-7777");
+		Assertions.assertEquals("1件挿入しました。", db.insert());
+	}
+}
